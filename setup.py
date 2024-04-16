ea6f7d5963729a8db440a7ddfcb8f992535cee72
Matthias Freioberger <m.freiberger@gmail.com>
2024-04-16 22:30:00 +0200

    updated Dockerfile and setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from setuptools import find_packages, setup
 setup(
     name='clipmasterprints',
     packages=find_packages(),
-    version='0.0.1',
-    description='Code base for CLIPMasterPrints: Fooling Contrastive Language-Image Pre-training Using Latent Variable Evolution',
+    version='0.0.2.2',
+    description='Supplementary code for \"Fooling Contrastive Language-Image Pre-trained Models with CLIPMasterPrints\"',
     author='Matthias Freiberger',
-    license='',
+    license='MIT',
     install_requires=[
-        "torch==1.11.0",
-        "torchvision==0.12.0",
+        "torch==2.0.1",
+        "torchvision==0.15.2",
         "numpy==1.23.3",
         "pandas==1.5.0",
         "cma==3.2.2",
         "yacs",
         "matplotlib==3.6.2",
-        "seaborn==0.12.1"
-
+        "seaborn==0.11",
+        "open-clip-torch>=2.23.0",
+        "timm==0.9.8",
+        "transformers==4.19.2"
     ]
 )
 
